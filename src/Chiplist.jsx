5d94e6341ba984672a5652db689785044bfcb819
Njogu Michael <michaelfebzen.mb@gmail.com>
2023-10-26 23:19:43 +0300

    Chips_Length

--- src/Chiplist.jsx
+++ src/Chiplist.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 
-function ChipList({ chips , maxChipsDisplayed }) {
+function ChipList({ chips , maxChipsDisplayed, maxTextLength }) {
    
-   
-    // console.log(maxChipsDisplayed)
-    const chiplist = chips.map(chip => <label key={chip.label}>{chip.label}</label>)
-    const trunc_chips = chiplist.slice(0,maxChipsDisplayed)
+    const chiplist = chips.map(chip => 
+                    {
+                        let stripped = chip.label.slice(0,maxTextLength)
+                
+                        if(chip.label.length > maxTextLength){
+                            let new_chip = stripped + "..."
+
+                            return <label key={chip.label}>{new_chip}</label>
+                        }
+                        else{
+                            return <label key={chip.label}>{chip.label}</label>
+                        }
+                    })
+
+    const truncated_chips = chiplist.slice(0,maxChipsDisplayed)
+    const exceedingText = chiplist.slice(maxChipsDisplayed)
+
+    const chipsToDisplay = chiplist.length > maxChipsDisplayed ? 
+                            <div>
+                                <p>{truncated_chips}</p>
+                                <aside data-testid="exceeding-text"> + {exceedingText.length} more items </aside>
+                            </div> : chiplist
+    
   
   return (
     <>
-        <div>{trunc_chips}</div>
+        {chipsToDisplay}
     </>
   );
 }
